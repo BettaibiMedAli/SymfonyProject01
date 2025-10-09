1a9d4b231f0b2cb9ff990737f55584e73d335f5a
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2025-10-09 11:54:00 +0100

    now we can delete, see and add an author

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Author;
+use App\Form\AuthorType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 final class AuthController extends AbstractController
 {
@@ -26,10 +31,61 @@ final class AuthController extends AbstractController
     }
 
     #[Route('/all', name : 'show_all')]
-    public function ShowAll(AuthorRepository $authorRepository){
+    public function showAll(AuthorRepository $authorRepository){
         $authors = $authorRepository->findAll();
         return $this->render('auth/all.html.twig',
         ['authors' => $authors]
         );
     }
+
+    #[Route('/addStat', name : 'addStat')]
+    public function addStat(ManagerRegistry $doctrine){
+        $author = new Author();
+        author->setEmail('test@gmail.com');
+        author->setUsername('foulen');
+
+        $em = $doctrine->getManager();
+        $em -> persist($author);
+        $em -> flush();
+
+        return $this -> redirectToRoute('show_all');
+    }
+
+    #[Route('/addForm', name : 'addForm')]
+    public function addForm(Request $request, ManagerRegistry $doctrine){
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->add('add',SubmitType::class);
+        $form->handleRequest($request);
+        
+        if($form -> isSubmitted()){
+            $em = $doctrine->getManager();
+            $em -> persist($author);
+            $em -> flush();
+
+            return $this->redirectToRoute('show_all');
+        }
+
+        return $this -> render('auth/add.html.twig',['formulaire'=>$form->createView()]);
+    }
+
+    #[Route('/deleteAuthor/{id}', name : 'deleteAuthor')]
+    public function deleteAuthor($id, AuthorRepository $repo, ManagerRegistry $manager){
+        $author = $repo -> find($id);
+        $em = $manager->getManager();
+
+        $em->remove($author);
+        $em->flush();
+
+        return $this -> redirectToRoute('show_all');
+    }
+
+    #[Route('/authorDetails/{id}', name : 'authorDetails')]
+    public function showAuthorDetails($id, AuthorRepository $repo){
+        $author = $repo -> find($id);
+
+        return $this -> render('auth/showDetails.html.twig',['author'=>$author]);
+    }
+
+
 }
